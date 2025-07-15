e0ae48eb80a0631858d665612671d79d87ca40ec
Laurence Morgan <mail@laurencemorgan.co.uk>
2025-07-15 09:13:08 +0100

    chore: update description

--- internal/meta.go
+++ internal/meta.go
@@ -5,4 +5,4 @@ const Name = "web scraper"
 const Description = `Downloads a web page and returns the contents of its page.
 Useful for checking online content.
 The input for this tool is a URL to the web page.
-The output of this tool will either be HTML.`
+The output of this tool will be HTML.`
